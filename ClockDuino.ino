ede3f85cb80e058cd88973ed4138d24d8bfd308f
lyj00912 <3122742760@qq.com>
2020-04-09 13:34:27 +0800

    create a timer use millis()

---
+++ ClockDuino.ino
@@ -0,0 +1,12 @@
+long a;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  a = millis();
+  Serial.println(a);
+}
